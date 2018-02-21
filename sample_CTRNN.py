011c5e0f7433743d793bfbc1b55f88a964e75e9a
Mostafa <mostafa.rafaie@gmail.com>
2018-02-21 10:55:39 -0600

    resolve some bugs

--- sample_CTRNN.py
+++ sample_CTRNN.py
@@ -1,20 +1,19 @@
 from CTRNN import CTRNN
 
-
 if __name__ == "__main__":
     run_duration = 250
     step_size = 0.01
 
     c = CTRNN(2)
-    c.set_neuron_bias(1, -2.75)
-    c.set_neuron_bias(2, -1.75)
+    c.set_neuron_bias(0, -2.75)
+    c.set_neuron_bias(1, -1.75)
+    c.set_connection_weight(0, 0, 4.5)
+    c.set_connection_weight(0, 1, -1)
+    c.set_connection_weight(1, 0, 1)
     c.set_connection_weight(1, 1, 4.5)
-    c.set_connection_weight(1, 2, -1)
-    c.set_connection_weight(2, 1, 1)
-    c.set_connection_weight(2, 2, 4.5)
 
     c.randomize_circuit_state(-0.5, 0.5)
-    print(0.0, c.neuron_output(1), c.neuron_output(2))
+    print(0.0, c.neuron_output(0), c.neuron_output(1))
     for time in range(int(run_duration/step_size)):
         c.euler_step(step_size)
-        print(time*step_size, c.neuron_output(1), c.neuron_output(2))
+        print(round(time*step_size,2), c.neuron_output(0), c.neuron_output(1))
