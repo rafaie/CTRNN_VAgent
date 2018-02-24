de7a8b7daac28435a415dc9d52aa6a501582b0e3
Mostafa <mostafa.rafaie@gmail.com>
2018-02-24 15:59:28 -0600

    Add interception Test

--- test_visual_agent.py
+++ test_visual_agent.py
@@ -8,14 +8,24 @@ from visual_agent import VisualAgent
 
 
 class TestVisualAgent(unittest.TestCase):
-    def test_intersection(self):
+    def calc_external_input(self, r):
+        return VisualAgent.INPUT_GAIN * \
+            (VisualAgent.MAX_RAY_LENGTH - r.length)/VisualAgent.MAX_RAY_LENGTH
 
+    def test_intersection(self):
         r = Ray(b=-111.962, m=-3.73205, startX=-33.8823, startY=14.4889,
                 length=220)
         l = Line(cx=30, cy=150, vy=-3, size=30)
-
         l.ray_intersection(r)
 
-        external_input = VisualAgent.INPUT_GAIN * \
-            (VisualAgent.MAX_RAY_LENGTH - r.length)/VisualAgent.MAX_RAY_LENGTH
-        print(' ==> ', external_input)
+        external_input = self.calc_external_input(r)
+        self.assertEqual(external_input, 0)
+
+        r = Ray(b=-111.962, m=-3.73205, startX=-33.8823, startY=14.4889,
+                length=220)
+        c = Circle(cx=30, cy=150, vy=-3, size=30)
+        c.ray_intersection(r)
+
+        external_input = self.calc_external_input(r)
+        self.assertEqual(external_input, 0)
+        # print(' ==> ', external_input)
