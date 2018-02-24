134e34601fcde9935879ffd6e434b45cd33764ea
Mostafa <mostafa.rafaie@gmail.com>
2018-02-23 22:28:09 -0600

    Add unit test for visual agent

---
+++ test_visual_agent.py
@@ -0,0 +1,21 @@
+import unittest
+from visual_object import Ray, Line, Circle
+from visual_agent import VisualAgent
+
+# import math
+# from utils import sigmoid, inverse_sigmoid
+# from CTRNN import CTRNN
+
+
+class TestVisualAgent(unittest.TestCase):
+    def test_intersection(self):
+
+        r = Ray(b=-111.962, m=-3.73205, startX=-33.8823, startY=14.4889,
+                length=220)
+        l = Line(cx=30, cy=150, vy=-3, size=30)
+
+        l.ray_intersection(r)
+
+        external_input = VisualAgent.INPUT_GAIN * \
+            (VisualAgent.MAX_RAY_LENGTH - r.length)/VisualAgent.MAX_RAY_LENGTH
+        print(' ==> ', external_input)
