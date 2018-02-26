992dd2e11ee9a046dcb5cbcba4f1cc1e1774e39b
Mostafa <mostafa.rafaie@gmail.com>
2018-02-26 12:48:03 -0600

    Complete main and Random function

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,5 @@
 import math
-
+import random
 
 BODY_SIZE = 30.0
 ENV_WIDTH = 400.0
@@ -9,33 +9,43 @@ ENV_MAX_HEIGHT = 400.0
 LINE = 0
 CIRCLE = 1
 
-SEQUNTIAL_DATA_COUNT = 64
-RANDOM_DATA_COUNT = 20
+HIGHT_DIST = 3
+WIDTH_DIST = 5
+RANDOM_DATA_COUNT = 10
 
 
-def gen_sequential_data(x, y, type=LINE):
+def gen_data(x1, x2, y2, type=LINE):
     pass
 
 
-def get_random_XY():
-    pass
+def get_random_X12Y():
+    x1 = random.randint(-ENV_WIDTH / 2 * BODY_SIZE/2,
+                        ENV_WIDTH / 2 * BODY_SIZE/2)
+    x2 = random.randint(-ENV_WIDTH / 2 * BODY_SIZE/2,
+                        ENV_WIDTH / 2 * BODY_SIZE/2)
+    y = random.randint()
+
+    return (x1, x2, y)
 
 
 if __name__ == '__main__':
     with open('dataset.csv', 'w') as fi:
-        for i in range(math.sqrt(SEQUNTIAL_DATA_COUNT)):
-            x = int(-ENV_WIDTH / 2 + (ENV_WIDTH - 2 * BODY_SIZE) /
-                    math.sqrt(SEQUNTIAL_DATA_COUNT) * i)
+        for i in range(WIDTH_DIST):
+            x1 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
+                     (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * i)
+            for j in range(WIDTH_DIST):
+                x2 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
+                         (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * j)
 
-            for j in range(math.sqrt(SEQUNTIAL_DATA_COUNT)):
-                y = int(ENV_MAX_HEIGHT - (ENV_MAX_HEIGHT - ENV_MIN_HEIGHT) /
-                        math.sqrt(SEQUNTIAL_DATA_COUNT) * j)
+                for k in range(HIGHT_DIST):
+                    y = int(ENV_MAX_HEIGHT -
+                            (ENV_MAX_HEIGHT - ENV_MIN_HEIGHT) / HIGHT_DIST * k)
 
-                fi.write(gen_sequential_data(x, y, LINE))
-                fi.write(gen_sequential_data(x, y, CIRCLE))
+                    fi.write(gen_data(x1, x2, y, LINE))
+                    fi.write(gen_data(x1, x2, y, CIRCLE))
 
         for i in range(RANDOM_DATA_COUNT):
-            x, y = get_random_XY()
+            x1, x2, y = get_random_X12Y()
 
-            fi.write(gen_sequential_data(x, y, LINE))
-            fi.write(gen_sequential_data(x, y, CIRCLE))
+            fi.write(gen_data(x1, x2, y, LINE))
+            fi.write(gen_data(x1, x2, y, CIRCLE))
