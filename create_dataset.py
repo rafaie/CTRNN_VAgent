17b163a9bfe62420a55df8f15e92906f77b36c61
Mostafa <mostafa.rafaie@gmail.com>
2018-02-26 13:04:48 -0600

    Complete Create Dataset file

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,5 @@
-import math
 import random
+import csv
 
 BODY_SIZE = 30.0
 ENV_WIDTH = 400.0
@@ -16,7 +16,7 @@ RANDOM_DATA_COUNT = 10
 
 def gen_data(x1, x2, y2, type=CIRCLE):
     y1 = 0
-    if CIRCLE:
+    if type == CIRCLE:
         return (CIRCLE, x1, y1, x2, y2, x2, 0)
     else:
         if abs(x2 - x1) > BODY_SIZE * 2:
@@ -39,6 +39,8 @@ def get_random_X12Y():
 
 if __name__ == '__main__':
     with open('dataset.csv', 'w') as fi:
+        outfile_csv = csv.writer(fi, delimiter=',',
+                                 quotechar="'", quoting=csv.QUOTE_MINIMAL)
         for i in range(WIDTH_DIST):
             x1 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
                      (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * i)
@@ -50,11 +52,11 @@ if __name__ == '__main__':
                     y = int(ENV_MAX_HEIGHT -
                             (ENV_MAX_HEIGHT - ENV_MIN_HEIGHT) / HIGHT_DIST * k)
 
-                    fi.write(gen_data(x1, x2, y, LINE))
-                    fi.write(gen_data(x1, x2, y, CIRCLE))
+                    outfile_csv.writerow(gen_data(x1, x2, y, LINE))
+                    outfile_csv.writerow(gen_data(x1, x2, y, CIRCLE))
 
         for i in range(RANDOM_DATA_COUNT):
             x1, x2, y = get_random_X12Y()
 
-            fi.write(gen_data(x1, x2, y, LINE))
-            fi.write(gen_data(x1, x2, y, CIRCLE))
+            outfile_csv.writerow(gen_data(x1, x2, y, LINE))
+            outfile_csv.writerow(gen_data(x1, x2, y, CIRCLE))
