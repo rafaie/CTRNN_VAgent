d202dfe647b3813e6a71cc5ed413d268db55e962
Mostafa <mostafa.rafaie@gmail.com>
2018-03-05 11:18:50 -0600

    add load_dataset function

--- train_agent.py
+++ train_agent.py
@@ -7,10 +7,25 @@ parameters for the CTRNN
 import logging.config
 import yaml
 import time
+import csv
 from genetic_algorithm.genetic_algorithm import GeneticAlgorithm
 
 __author__ = "Mostafa Rafaie"
 __license__ = "APLv2"
+
+# dataset
+dataset_path = 'dataset.csv'
+dataset = []
+
+
+def load_dataset():
+    dataset = []
+
+    with open('dataset.csv', 'r') as fi:
+        csv_file = csv.reader(fi, delimiter=',',
+                              quotechar="'", quoting=csv.QUOTE_MINIMAL)
+        for row in csv_file:
+            dataset.append(row)
 
 
 # Using inverse function for fitness 1/F(x)
@@ -30,15 +45,17 @@ if __name__ == "__main__":
     crossover_type = GeneticAlgorithm.TWO_POINT_CROSSOVER
     fitness_goal = 0.00001
 
-    ga = GeneticAlgorithm(path)
+    load_dataset()
 
-    start_time = time.time()
-    population = ga.run(init_population_size, population_size,
-                        mutation_rate, num_iteratitions, crossover_type,
-                        calc_fitness, fitness_goal,
-                        cuncurrency=1,
-                        reverse_fitness_order=False)
-    end_time = time.time()
-    print(population[:3].astype(float))
-    print(population[:, -1].astype(float))
-    print('Runtime :', end_time - start_time)
+    # ga = GeneticAlgorithm(path)
+    #
+    # start_time = time.time()
+    # population = ga.run(init_population_size, population_size,
+    #                     mutation_rate, num_iteratitions, crossover_type,
+    #                     calc_fitness, fitness_goal,
+    #                     cuncurrency=1,
+    #                     reverse_fitness_order=False)
+    # end_time = time.time()
+    # print(population[:3].astype(float))
+    # print(population[:, -1].astype(float))
+    # print('Runtime :', end_time - start_time)
