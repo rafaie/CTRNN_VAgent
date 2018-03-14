3dead82b69402347efdd9b8c409e29d73300c844
Mostafa <mostafa.rafaie@gmail.com>
2018-03-14 18:55:13 -0500

    Update dataset!

--- create_dataset.py
+++ create_dataset.py
@@ -6,15 +6,15 @@ ENV_WIDTH = 400.0
 ENV_MIN_HEIGHT = 200.0
 ENV_MAX_HEIGHT = 400.0
 ENV_BASE_HEIGHT = 250.0
-ENV_MIN_X = -25
-ENV_MAX_X = 25
+ENV_MIN_X = -30
+ENV_MAX_X = 30
 
 
 LINE = 1
 CIRCLE = 2
 
 HIGHT_DIST = 3
-WIDTH_DIST = 4
+WIDTH_DIST = 5
 RANDOM_DATA_COUNT = 2
 
 
@@ -49,7 +49,7 @@ if __name__ == '__main__':
             # x1 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
             #          (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * i)
             dist = ENV_MAX_X - ENV_MIN_X
-            x1 = int(ENV_MIN_X + dist / (WIDTH_DIST - 1) * i)
+            x1 = 3 * int(ENV_MIN_X + dist / (WIDTH_DIST - 1) * i)
             for j in range(WIDTH_DIST):
                 # x2 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
                 #          (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * j)
