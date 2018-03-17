a35e0a1a458ce07bfb8534848073414bee97e898
Mostafa <mostafa.rafaie@gmail.com>
2018-03-17 00:18:38 -0500

    Update dataset again!

--- create_dataset.py
+++ create_dataset.py
@@ -5,16 +5,18 @@ BODY_SIZE = 30.0
 ENV_WIDTH = 400.0
 ENV_MIN_HEIGHT = 200.0
 ENV_MAX_HEIGHT = 400.0
-ENV_BASE_HEIGHT = 250.0
+ENV_BASE_HEIGHT = 200.0
 ENV_MIN_X = -30
 ENV_MAX_X = 30
-
+ENV_OBJ_X_MOVEMENT = 30
+ENV_AGENT_X_MOVEMENT = 100
 
 LINE = 1
 CIRCLE = 2
 
 HIGHT_DIST = 3
-WIDTH_DIST = 5
+WIDTH_DIST = 7
+WIDTH_DIST2 = 3
 RANDOM_DATA_COUNT = 2
 
 
@@ -48,12 +50,14 @@ if __name__ == '__main__':
         for i in range(WIDTH_DIST):
             # x1 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
             #          (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * i)
-            dist = ENV_MAX_X - ENV_MIN_X
-            x1 = 2 * int(ENV_MIN_X + dist / (WIDTH_DIST - 1) * i)
-            for j in range(WIDTH_DIST):
+            # dist = ENV_MAX_X - ENV_MIN_X
+            x1 = int(-ENV_AGENT_X_MOVEMENT / 2.0 +
+                     (i) * ENV_AGENT_X_MOVEMENT / (WIDTH_DIST-1))
+            for j in range(WIDTH_DIST2):
                 # x2 = int((-ENV_WIDTH / 2 + BODY_SIZE/2) +
                 #          (ENV_WIDTH - BODY_SIZE) / WIDTH_DIST * j)
-                x2 = int(ENV_MIN_X + dist / (WIDTH_DIST - 1) * j)
+                x2 = int(-ENV_OBJ_X_MOVEMENT / 2.0 +
+                         (i) * ENV_OBJ_X_MOVEMENT / (WIDTH_DIST-1))
 
                 # for k in range(HIGHT_DIST):
                 #     y = int(ENV_MAX_HEIGHT -
